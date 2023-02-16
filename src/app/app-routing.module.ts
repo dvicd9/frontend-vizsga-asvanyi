37c8db6f9cd730272eeffd2bc62a068cc20ef62d
Gregor <dvicd9@inf.elte.hu>
2023-02-16 09:26:59 +0100

    Frame and Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoryComponent } from './history/history.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'history', component: HistoryComponent},
 ];
 
 @NgModule({
