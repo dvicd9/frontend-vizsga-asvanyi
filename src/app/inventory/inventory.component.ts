49a9a551780586687ff8898c02f2305c667d1e55
Gregor <dvicd9@inf.elte.hu>
2023-02-16 11:40:12 +0100

    Final

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -9,14 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoryComponent implements OnInit{
   inventory: any[] = [];
+  isLoaded: boolean = false;
 
   constructor(private http: HttpClient){}
 
   ngOnInit(){
     this.http.get<any>('https://infra-hulling-376212-default-rtdb.europe-west1.firebasedatabase.app/plushies.json')
     .subscribe( (inventory: any[]) => {
+      this.isLoaded = true;
       this.inventory = inventory;
-      console.log(inventory);
     });
   }
 }
